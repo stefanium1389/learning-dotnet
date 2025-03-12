57d15e52011a3e58de4b650480cc8468c091c292
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2025-03-12 14:42:40 +0100

    Add files via upload

--- Projekat/Infrastructure/Presentation/Controllers/CommentController.cs
+++ Projekat/Infrastructure/Presentation/Controllers/CommentController.cs
@@ -1,5 +1,9 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service.Abstractions;
+using Shared.Dtos;
 
 namespace Presentation.Controllers
 {
@@ -7,5 +11,35 @@ namespace Presentation.Controllers
     [ApiController]
     public class CommentController : ControllerBase
     {
+        private readonly IServiceManager _serviceManager;
+        public CommentController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Tourist")]
+        public async Task<IActionResult> PostCommentAsync([FromBody] CommentCreationDto dto)
+        {
+            string? userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            CommentDto commentDto = await _serviceManager.CommentService.CreateCommentAsync(dto, userId!);
+            return Ok(commentDto);
+        }
+        [HttpPatch]
+        [Route("{id}/approve")]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> ApproveComment([FromRoute] string id)
+        {
+            CommentDto commentDto = await _serviceManager.CommentService.ApproveCommentAsync(id);
+            return Ok(commentDto);
+        }
+        [HttpPatch]
+        [Route("{id}/decline")]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> DeclineComment([FromRoute] string id)
+        {
+            CommentDto commentDto = await _serviceManager.CommentService.DeclineCommentAsync(id);
+            return Ok(commentDto);
+        }
     }
 }
